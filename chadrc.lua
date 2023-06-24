23fa0b9f46891d81449b7599b9d288f980ce9623
Kelvin Chin <chinkaiwen@yahoo.com>
2023-06-25 00:41:29 +0800

    Initialized

---
+++ chadrc.lua
@@ -0,0 +1,9 @@
+---@type ChadrcConfig 
+ local M = {}
+ M.ui = {
+  theme = 'github_dark',
+  transparency = true
+}
+M.plugins = 'custom.plugins'
+
+return M
---
+++ init.lua
@@ -0,0 +1,34 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.colorcolumn = "110"
+vim.opt.visualbell = true
+
+vim.api.nvim_set_keymap('n', 'dd', '"_dd', { noremap = true })
+vim.api.nvim_set_keymap('n', 'x', '"_x', { noremap = true })
+vim.api.nvim_set_keymap('n', 'd', '"_d', { noremap = true })
+vim.api.nvim_set_keymap('n', 'D', '"_D', { noremap = true })
+vim.api.nvim_set_keymap('n', 's', 'i<CR><Esc>', { noremap = true })
+vim.api.nvim_set_keymap('n', 'S', 'a<CR><Esc>', { noremap = true })
+vim.api.nvim_set_keymap('v', 'd', '"_d', { noremap = true })
+
+vim.api.nvim_set_keymap('n', '<leader>dd', '"*dd', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>d', '"*d', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>D', '"*D', { noremap = true })
+vim.api.nvim_set_keymap('v', '<leader>d', '"*d', { noremap = true })
+
+vim.api.nvim_set_keymap('n', 'yy', '"*yy', { noremap = true })
+vim.api.nvim_set_keymap('n', 'y', '"*y', { noremap = true })
+vim.api.nvim_set_keymap('n', 'p', '"*p', { noremap = true })
+vim.api.nvim_set_keymap('n', 'P', '"*P', { noremap = true })
+
+vim.api.nvim_set_keymap('n', '<leader>hl', ':set hlsearch!<cr>', { noremap = true })
+
+-- Auto format on save
+vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '',
+    command = ':Neoformat'
+})

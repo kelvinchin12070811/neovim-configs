2330cf84bdd6b4a1252a935ed9598561e4b94a7c
Kelvin Chin <chinkaiwen@yahoo.com>
2023-07-14 03:09:21 +0800

    Added prettier and fixed some error

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,12 +12,3 @@ vim.opt.expandtab = true
 vim.opt.colorcolumn = "110"
 vim.opt.visualbell = true
 vim.opt.clipboard = nil
-
-vim.cmd([[hi Normal guibg=none ctermbg=none]])
-vim.cmd([[hi LineNr guibg=none ctermbg=none]])
-vim.cmd([[hi Folded guibg=none ctermbg=none]])
-vim.cmd([[hi NonText guibg=none ctermbg=none]])
-vim.cmd([[hi SpecialKey guibg=none ctermbg=none]])
-vim.cmd([[hi VertSplit guibg=none ctermbg=none]])
-vim.cmd([[hi SignColumn guibg=none ctermbg=none]])
-vim.cmd([[hi EndOfBuffer guibg=none ctermbg=none]])
